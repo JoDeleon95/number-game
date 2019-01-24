aa3a5285f1b62137df787fdc616a03187eae381b
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-01-24 15:40:40 -0700

    Refactored and added gitignore.

---
+++ src/edu/cnm/deepdive/NumberGame.java
@@ -0,0 +1,40 @@
+package edu.cnm.deepdive;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+
+  private static Random random = new Random();
+  private static Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    boolean playAgain;
+    int secret;
+    int guess;
+
+    secret = random.nextInt(100) + 1;
+    System.out.println("Guess a number between 1 - 100: ");
+    do {
+      guess = input.nextInt();
+      if (guess == 0) {
+        System.out.println("All you had to do was follow the train.");
+      }
+      if (guess > secret) {
+        System.out.println("Too damn high");
+        System.out.println("Guess again");
+      } else if (guess < secret) {
+        System.out.println("Too low nerd.");
+        System.out.println("Guess again");
+      }
+
+    } while (guess != secret);
+    System.out.println("congratulation, you done it.");
+    System.out.println(secret);
+    System.out.println("Play again?: ");
+
+
+  }
+
+}
